f0363e1ebe5d1c962c740ee6e8395a8b563e65a0
Philipp Kinkel <>
2026-06-18 14:00:12 +0200

    Auto-reload app when new service worker activates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'betterkerf-20260618-2';
+const CACHE = 'betterkerf-20260618-3';
 const ASSETS = [
   './betterKerf.html',
   './manifest.json',
@@ -15,7 +15,11 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
+    .then(() => self.clients.claim())
+    .then(() => self.clients.matchAll({ type: 'window' }).then(clients =>
+      clients.forEach(c => c.postMessage({ type: 'UPDATE' }))
+    ))
   );
 });
 
